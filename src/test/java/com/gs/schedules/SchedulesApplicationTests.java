16ce38fdadc09e365592bf2a8c75c61e38c60835
gaddamb <36534803+gaddamb@users.noreply.github.com>
2022-08-23 23:58:48 +0200

    Initial Commit

---
+++ src/test/java/com/gs/schedules/SchedulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gs.schedules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
