82dc57371cc438d2ea34d0691b01fb44d1dd6bb3
gaddamb <36534803+gaddamb@users.noreply.github.com>
2022-08-23 23:55:42 +0200

    Initial Commit

---
+++ src/main/java/com/gs/schedules/controller/ZoneController.java
@@ -0,0 +1,33 @@
+package com.gs.schedules.controller;
+
+import com.gs.schedules.entity.Zone;
+import com.gs.schedules.model.ZoneDTO;
+import com.gs.schedules.service.ZoneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/zone")
+public class ZoneController {
+
+    @Autowired
+    private ZoneService zoneService;
+
+    @PostMapping("/")
+    public Zone createZone(@RequestBody Zone zone) {
+        return zoneService.saveZone(zone);
+    }
+
+    @PostMapping("/configureSchedule")
+    public Zone configureZoneToSchedule(@RequestBody ZoneDTO zone) {
+        return zoneService.configureSchedule(zone);
+    }
+
+    @PostMapping("/verifyStatus")
+    public String scheduleForZone(@RequestBody ZoneDTO zone) {
+        return zoneService.verifyStatus(zone);
+    }
+}
