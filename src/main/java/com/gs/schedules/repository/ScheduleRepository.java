ac90b0c58554ffdbf60af06309158fbcefb138e5
gaddamb <36534803+gaddamb@users.noreply.github.com>
2022-08-25 00:43:58 +0200

    Added method for fetching schedule item based on zone

--- src/main/java/com/gs/schedules/repository/ScheduleRepository.java
+++ src/main/java/com/gs/schedules/repository/ScheduleRepository.java
@@ -1,12 +1,16 @@
 package com.gs.schedules.repository;
 
 import com.gs.schedules.entity.Schedule;
+import com.gs.schedules.entity.Zone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     Schedule findByName(String name);
+
+    Schedule findByZone(Zone zone);
 
 }
