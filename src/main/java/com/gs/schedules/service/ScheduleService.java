58bc494f06d44e229e8d2791964fb5bb80313f7f
gaddamb <36534803+gaddamb@users.noreply.github.com>
2022-08-25 01:04:31 +0200

    removed unused imports

--- src/main/java/com/gs/schedules/service/ScheduleService.java
+++ src/main/java/com/gs/schedules/service/ScheduleService.java
@@ -1,9 +1,6 @@
 package com.gs.schedules.service;
 
-import com.gs.schedules.entity.Schedule;
-import com.gs.schedules.entity.ScheduleItem;
 import com.gs.schedules.model.ScheduleDTO;
-import java.util.List;
 
 public interface ScheduleService {
 
