7c3ae675d4c8d6ff884abdc444dd26a3d746dbc7
gaddamb <36534803+gaddamb@users.noreply.github.com>
2022-08-25 00:45:23 +0200

    Update ScheduleService.java

--- src/main/java/com/gs/schedules/service/ScheduleService.java
+++ src/main/java/com/gs/schedules/service/ScheduleService.java
@@ -1,11 +1,13 @@
 package com.gs.schedules.service;
 
 import com.gs.schedules.entity.Schedule;
+import com.gs.schedules.entity.ScheduleItem;
 import com.gs.schedules.model.ScheduleDTO;
+import java.util.List;
 
 public interface ScheduleService {
 
     public ScheduleDTO createSchedule(ScheduleDTO scheduleDTO);
 
-    public Schedule updateSchedule(ScheduleDTO schModel);
+    public ScheduleDTO updateSchedule(ScheduleDTO schModel);
 }
