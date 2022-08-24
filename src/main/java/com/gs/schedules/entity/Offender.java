8e96f6f39bc250b363168d33880b0c4bb673b672
gaddamb <36534803+gaddamb@users.noreply.github.com>
2022-08-25 00:38:47 +0200

    Update Offender.java

--- src/main/java/com/gs/schedules/entity/Offender.java
+++ src/main/java/com/gs/schedules/entity/Offender.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Offender {
 
     @Id
