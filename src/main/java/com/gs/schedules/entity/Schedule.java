baba692c9d72d6c87a14330049a0dc9fa6f7f297
gaddamb <36534803+gaddamb@users.noreply.github.com>
2022-08-25 00:39:06 +0200

    Update Schedule.java

--- src/main/java/com/gs/schedules/entity/Schedule.java
+++ src/main/java/com/gs/schedules/entity/Schedule.java
@@ -1,10 +1,13 @@
 package com.gs.schedules.entity;
 
+import java.util.Date;
 import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Data
@@ -39,12 +42,13 @@ public class Schedule {
     )
     private Zone zone;
 
-    /*@OneToMany(
-            cascade = CascadeType.ALL
-    )
-    @JoinColumn(
-            name = "schedule_id",
-            referencedColumnName = "scheduleId"
-    )
-    private List<ScheduleItem> scheduleItemList = new ArrayList<>(); */
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_date")
+    private Date createDate;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modify_date")
+    private Date modifyDate;
 }
