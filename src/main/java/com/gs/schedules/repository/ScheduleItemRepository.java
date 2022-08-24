8bfb5c8add8ab1d7e9f492125ee7438304393eea
gaddamb <36534803+gaddamb@users.noreply.github.com>
2022-08-25 00:43:21 +0200

    Added method for fetching the schedule

--- src/main/java/com/gs/schedules/repository/ScheduleItemRepository.java
+++ src/main/java/com/gs/schedules/repository/ScheduleItemRepository.java
@@ -1,7 +1,14 @@
 package com.gs.schedules.repository;
 
+import com.gs.schedules.entity.Schedule;
 import com.gs.schedules.entity.ScheduleItem;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ScheduleItemRepository extends JpaRepository<ScheduleItem, Long> {
+
+
+    @Query("select si from ScheduleItem si join fetch si.schedule where si.schedule = ?1")
+    List<ScheduleItem> findSchedule(Schedule schedule);
 }
